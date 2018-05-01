9f0f0ddcb3187a7f5caf8facea84728b543d73fe
alexm-status <38202046+alexm-status@users.noreply.github.com>
2018-04-30 22:11:32 -0700

    Update siteConfig.js

--- build/test-site/siteConfig.js
+++ build/test-site/siteConfig.js
@@ -21,15 +21,15 @@ const users = [
 const siteConfig = {
   title: 'Status / Nim Docs' /* title for your website */,
   tagline: 'Testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'https://status.github.io/' /* your website url */,
+  baseUrl: 'docs/build/test-site/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'test-site',
-  organizationName: 'facebook',
+  organizationName: 'status-im',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
